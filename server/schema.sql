4d4245a25f53a6d8d84d81f282d270c24ce07185
doodoodoom <madridmatt17@gmail.com>
2019-01-25 19:02:31 -0800

    Implement new table schemas

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,31 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE rooms (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE usernames (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE texts (
+  id INTEGER PRIMARY KEY,
+  message TEXT NOT NULL,
+  username INTEGER,
+  room INTEGER,
+  FOREIGN KEY(username) REFERENCES usernames(id),
+  FOREIGN KEY(room) REFERENCES rooms(id)
+);
+
+CREATE TABLE rooms_usernames (
+  id INTEGER PRIMARY KEY,
+  room_id INTEGER,
+  username_id INTEGER,
+  FOREIGN KEY(username_id) REFERENCES usernames(id),
+  FOREIGN KEY(room_id) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */

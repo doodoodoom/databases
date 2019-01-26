7c2b61644132cf657fa99d50423f6fe83c5d32aa
doodoodoom <madridmatt17@gmail.com>
2019-01-25 22:20:52 -0800

    Implement get functionality to sendQuery

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,36 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (req, res) {
+
+      var query = 'SELECT * FROM texts';
+      db.sendQuery.query(query, (err, results, fields) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.status(200).send(JSON.stringify(results));
+        }
+      });
+
+    }, // a function which produces all the messages
+
+
+    post: function () {
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+
+    },
+
+
+
+    post: function () {
+
+    }
   }
 };
 

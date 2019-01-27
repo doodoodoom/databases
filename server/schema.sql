5d030eb97ae91db332437fb909a4948d645516fb
doodoodoom <madridmatt17@gmail.com>
2019-01-26 16:02:39 -0800

    Refactor schemas to usernames and messages table

--- server/schema.sql
+++ server/schema.sql
@@ -2,31 +2,16 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE rooms (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  name TEXT
-);
-
 CREATE TABLE usernames (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   name TEXT
 );
 
-CREATE TABLE texts (
+CREATE TABLE messages (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  message TEXT,
-  username INTEGER,
-  room INTEGER,
-  FOREIGN KEY(username) REFERENCES usernames(id),
-  FOREIGN KEY(room) REFERENCES rooms(id)
-);
-
-CREATE TABLE rooms_usernames (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  room_id INTEGER,
-  username_id INTEGER,
-  FOREIGN KEY(username_id) REFERENCES usernames(id),
-  FOREIGN KEY(room_id) REFERENCES rooms(id)
+  text TEXT,
+  roomname TEXT,
+  username INTEGER
 );
 
 /* Create other tables and define schemas for them here! */
